51de3002d2ff55e580127b89247dcf65f71f8f6a
Henry <henry_buschmann@web.de>
2022-06-17 15:22:31 +0200

    clear section button
    button, which clears the "done" section

--- ShoppingList/ShoppingList/ContentView.swift
+++ ShoppingList/ShoppingList/ContentView.swift
@@ -13,35 +13,43 @@ struct ContentView: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     @State private var number = 0
+    @State private var disabled = true
     
     var body: some View {
-        NavigationView {
-            List {
-                Section {
-                    TextField("Enter your word", text: $newWord)
-                }
-                
-                Section(header: Text("not done")) {
-                    ForEach(usedWords, id: \.self) { word in
-                        Text(word)
+            NavigationView {
+                List {
+                    Section {
+                        TextField("Enter your word", text: $newWord)
                     }
-                    .onDelete(perform: delete)
-                }
-                
-                Section(header: Text("done")) {
-                    ForEach(finishedWords, id: \.self) { word in
-                        Text(word)
-                    }
-                    .onDelete(perform: { indexSet in
-                        finishedWords.remove(atOffsets: indexSet)
-                    })
                     
+                    Section(header: Text("not done")) {
+                        ForEach(usedWords, id: \.self) { word in
+                            Text(word)
+                        }
+                        .onDelete(perform: delete)
+                        
+                    }
+                    
+                    Section(header: Text("done")) {
+                        ForEach(finishedWords, id: \.self) { word in
+                            Text(word)
+                        }
+                        .onDelete(perform: { indexSet in
+                            finishedWords.remove(atOffsets: indexSet)
+                            buttonVisibility()
+                        })
+                    }
+                    .listRowBackground(Color.init(red: 153 / 255, green: 255 / 255, blue: 204 / 255))
+                    
+                    Button("clear section") {
+                        finishedWords.removeAll()
+                        disabled = true
+                    }
+                    .listRowBackground(Color.init(red: 255 / 255, green: 153 / 255, blue: 153 / 255))
+                    .disabled(disabled)
                 }
-                .listRowBackground(Color.init(red: 153 / 255, green: 255 / 255, blue: 204 / 255))
-            }
-            .navigationTitle(rootWord)
-            .onSubmit(addNewWord)
-            
+                .navigationTitle(rootWord)
+                .onSubmit(addNewWord)
         }
     }
     
@@ -60,6 +68,16 @@ struct ContentView: View {
         let index = offsets[offsets.startIndex]
         finishedWords.insert(usedWords[index], at: 0)
         usedWords.remove(at: index)
+        buttonVisibility()
+    }
+    
+    func buttonVisibility() {
+        if(finishedWords.isEmpty) {
+            disabled = true
+        }
+        else {
+            disabled = false
+        }
     }
 }
 
